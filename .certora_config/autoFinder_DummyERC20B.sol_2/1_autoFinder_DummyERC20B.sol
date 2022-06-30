110ba7bc1e60df1a384c417f3a308844e8781194
Ikhlas <ikhlaskhan007@gmail.com>
2022-06-30 04:27:25 +0400

    June 30, 2022 4:27 AM

--- .certora_config/autoFinder_DummyERC20B.sol_2/1_autoFinder_DummyERC20B.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: agpl-3.0
-pragma solidity 0.6.12;
-
-import '/home/i/Desktop/Certora_Forked_AsTETH/aave-protocol-v2-AStETH/certora/harness/autoFinder_DummyERC20Impl.sol';
-
-contract DummyERC20B is DummyERC20Impl {}
